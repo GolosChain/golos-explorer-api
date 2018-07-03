7ffabc797ab4558c312f87ab814be2f502cb6912
Sing Painter <mail@erlan.xyz>
2018-07-03 12:03:46 +0300

    + enable CORS

--- api-explorer.js
+++ api-explorer.js
@@ -5,6 +5,12 @@
 
 	server.listen(8080);
 
+	app.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+		next();
+	});
+
 	app.get('/', (req, res) => {
 		res.send('API for explorer.golos.io');
 	});
