63656e2fb749e0ba053f1e85de05058c9a23101b
Sing Painter <mail@erlan.xyz>
2018-07-05 02:05:33 +0300

    + add getPosts

--- api-explorer.js
+++ api-explorer.js
@@ -16,12 +16,13 @@
 	});
 	
 	let getAccounts;
+	let getComments;
 
 	MongoClient.connect(config.mongoUrlConnect, { useNewUrlParser: true }, (err, client) => {
 		if ( ! err) {
 			const db = client.db('Golos');
-			const account_object = db.collection('account_object');
 			
+			const account_object = db.collection('account_object');
 			getAccounts = (query, callback) => {
 				account_object
 					.find(query.filterModel)
@@ -40,13 +41,46 @@
 						catch (e) {
 							//console.error(e);
 						}
-						return { name: account.name, post_count: account.post_count, vesting_shares_value: account.vesting_shares_value, balance_value: account.balance_value, sbd_balance_value: account.sbd_balance_value, profile_image: profile_image }
+						return { created: account.created, name: account.name, post_count: account.post_count, vesting_shares_value: account.vesting_shares_value, balance_value: account.balance_value, sbd_balance_value: account.sbd_balance_value, profile_image: profile_image }
 					}))
 					.then(accounts => {
 						account_object.countDocuments()
 							.then(count => {
 								if (Object.keys(query.filterModel).length != 0) count = accounts.length;
 								callback({ lastRow: count, rows: accounts });
+							});
+					});
+			}
+			
+			const comment_object = db.collection('comment_object');
+			getComments = (query, callback) => {
+				query.filterModel['parent_author'] = '';
+				comment_object
+					.find(query.filterModel)
+					.skip(query.startRow)
+					.limit(query.endRow)
+					.sort(query.sortModel)
+					.toArray(/* (err, accounts) => {
+						return accounts;
+					} */)
+					.then(accounts => accounts.map(comment => {
+						/* let profile_image = null;
+						try {
+							let jsonMetadata = JSON.parse(account.json_metadata);
+							if (jsonMetadata.profile && jsonMetadata.profile.profile_image) profile_image = jsonMetadata.profile.profile_image;
+						}
+						catch (e) {
+							//console.error(e);
+						} */
+						//console.log(comment);
+						return { created: comment.created, author: comment.author, permlink: comment.permlink, title: comment.title, total_payout_symbol: comment.total_payout_symbol, total_payout_value: comment.total_payout_value, parent_permlink: comment.parent_permlink }
+						//return comment;
+					}))
+					.then(comments => {
+						comment_object.countDocuments()
+							.then(count => {
+								if (Object.keys(query.filterModel).length != 0) count = comments.length;
+								callback({ lastRow: count, rows: comments });
 							});
 					});
 			}
@@ -86,6 +120,13 @@
 	app.get('/getAccounts', (req, res) => {
 		req.query = handlerReqAgGrid(req.query);
 		getAccounts(req.query, (accounts) => {
+			res.send(accounts);
+		});
+	});
+	
+	app.get('/getComments', (req, res) => {
+		req.query = handlerReqAgGrid(req.query);
+		getComments(req.query, (accounts) => {
 			res.send(accounts);
 		});
 	});
